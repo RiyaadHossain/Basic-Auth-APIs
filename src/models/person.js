1b80056ddec1bdc10b4483fcd9105342f8ec8919
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-07-07 09:05:04 +0600

    login api created

--- src/models/person.js
+++ src/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const PersonSchema = mongoose.Schema({
   firstName: {
@@ -18,12 +19,24 @@ const PersonSchema = mongoose.Schema({
   },
   password: {
     type: String,
-    min: [6, "Password length should be at least 6"]
+    min: [6, "Password length should be at least 6"],
+  },
+  confirmPassword: {
+    type: String,
+    min: [6, "Password length should be at least 6"],
   },
   gender: {
     type: String,
-    enum : ["Male", "Female"]
-  }
+    enum: ["Male", "Female"],
+  },
 });
 
-module.exports = mongoose.model("Person", PersonSchema)
+PersonSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.confirmPassword = undefined
+  }
+  next();
+});
+
+module.exports = mongoose.model("Person", PersonSchema);
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,42 +2,45 @@ const PersonModel = require("../models/person");
 const express = require("express");
 const { findOne } = require("../models/person");
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 router.post("/signup", async (req, res) => {
   try {
-
     // Check weather the email is exist or not
     const email = req.body.email;
-    const personExist = await PersonModel.findOne({email});
+    const personExist = await PersonModel.findOne({ email });
     if (personExist) {
       return res.status(401).json({ message: "Person Already Exist" });
     }
 
     // Create New Person
-    const newPerson = new PersonModel(req.body);
-    const personCreated = await newPerson.save();
-    return res.status(200).json(personCreated);
-
+    if (req.body.password === req.body.confirmPassword) {
+      const newPerson = new PersonModel(req.body);
+      const personCreated = await newPerson.save();
+      return res.status(200).json(personCreated);
+    } else {
+      return res.status(200).json({ message: "Password didn't match" });
+    }
   } catch (error) {
     return res.status(400).json(error);
   }
 });
 
-router.post("/signin", async(req, res) => {
+router.post("/signin", async (req, res) => {
   try {
-    const email = req.body.email
-    const password = req.body.password
-    const personInfo = await PersonModel.findOne({email})
-    
-    if(personInfo.password === password){
-      return res.status(200).json({message: "Person Log in Successfully"})
-    }
-    return res.status(301).json({message: "Person credentials didn't match"})
-    
-  } catch (error) {
-    res.status(401).json({message: "Something Went wrong"})
-  }
-})
+    const email = req.body.email;
+    const password = req.body.password;
+    const personInfo = await PersonModel.findOne({ email });
 
+    const passMatched = await bcrypt.compare(password, personInfo.password)
+
+    if (passMatched) {
+      return res.status(200).json({ message: "Person Log in Successfully" });
+    }
+    return res.status(301).json({ message: "Person credentials didn't match" });
+  } catch (error) {
+    res.status(401).json({ message: "Something Went wrong" });
+  }
+});
 
 module.exports = router;
